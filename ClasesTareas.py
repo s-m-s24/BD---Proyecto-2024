a9af5f593210c54098147765dbc73c6e1e170b94
Sofia Sama <>
2024-11-25 17:10:47 -0300

    Arregla errores en "F", ahora funciona todo

--- ClasesTareas.py
+++ ClasesTareas.py
@@ -54,18 +54,18 @@ def ingresarTareas(listaTareas,fechas,tiempoTareas):
         mes:int
         dia:int
         año: int
-        #dificultad: int
-        #materia:str
+        dificultad: int
+        materia:str
 
 
-        def __init__(self, nombre, duracion,dia,mes,año):#,dificultad,materia
+        def __init__(self, nombre, duracion,dia,mes,año,dificultad,materia):
             self.nombre = nombre
             self.duracion = duracion
             self.dia = dia
             self.mes = mes
             self.año = año
-            #self.dificultad = dificultad
-            #self.materia = materia
+            self.dificultad = dificultad
+            self.materia = materia
        
         def __getitem__(self, key):
         # Usamos un diccionario para hacer que los atributos sean accesibles por nombre
@@ -93,15 +93,12 @@ def ingresarTareas(listaTareas,fechas,tiempoTareas):
 
         def __str__(self):
             return f"Nombre: {self.nombre}, Duración: {self.duracion}h, Fecha: {self.dia}/{self.mes}/{self.año}"#, Dificultad: {self.dificultad}, Materia: {self.materia}
-
-
-    """
+    
     ingresar=input("¿Quieres ingresar una tarea?: ")
+    #print(ingresar)
 
-
-
-
-    while ingresar.lower() in ["sí", "si"] :
+    while ingresar.lower() in ["si","sí"] :
+        
         nombre = input("Ingrese el nombre de esta tarea: ")
 
 
@@ -110,76 +107,45 @@ def ingresarTareas(listaTareas,fechas,tiempoTareas):
             duracion=input("No ingresó un número, por favor ingrese un número: ")
         duracion=int(duracion)
 
-
         tiempoTareas=tiempoTareas+duracion
-
 
         valido=False
 
-
-
-
         while valido == False:
-
-
-
 
             mes = input("Ingrese el mes en que vence esta tarea: ")
             mes = FuncionesDeDatos.verificar_num(mes,12,1)
-
-
-
 
             diaMax = dias_por_mes[mes-1]
             dia = input("Ingrese el dia en que vence esta tarea: ")
             dia = FuncionesDeDatos.verificar_num(dia,diaMax,1)
 
-
-
-
             año = input("Ingrese el año en que vence esta tarea: ")
             año = FuncionesDeDatos.verificar_num(año,2025,2024)
 
-
-
-
             day = FuncionesDeDatos.obtener_dia_tareas(dia,mes,dias_por_mes)
-
-
-
-
             valido=True
-
-
-
 
             if año==2025:
                 day=day+365
-
-
-
 
             if today>day:
                 print("El día que has ingresado ya pasó, por favor reingresa la fecha")
                 valido=False
 
+        dificultad = input("Ingrese la dificultad de esta tarea en una escala del 1 al 10, en el que 1 es el más fácil y 10 el más difícil: ")
+        dificultad = FuncionesDeDatos.verificar_num(dificultad,10,1)
 
+        materia = input("Ingrese la materia de esta tarea: ")
 
+        tarea = Tareas(nombre,duracion,dia,mes,año,dificultad,materia)
+        
+        listaTareas=FuncionesDeDatos.ordenar_lista_tareas(listaTareas,fechas,day,tarea)
 
-        #dificultad = input("Ingrese la dificultad de esta tarea en una escala del 1 al 10, en el que 1 es el más fácil y 10 el más difícil: ")
-        #dificultad = FuncionesDeDatos.verificar_num(dificultad,10,1)
+        ingresar=input("¿Quieres ingresar una tarea?: ")
 
-
-        #materia = input("Ingrese la materia de esta tarea: ")
-
-
-        tarea = Tareas(nombre,duracion,dia,mes,año)#,dificultad,materia
-        ingresar=input("¿Querés ingresar otra tarea?: ")
-        """
-
-
-        #listaTareas=FuncionesDeDatos.ordenar_lista_tareas(listaTareas,fechas,day,tarea)
-
+    return listaTareas, fechas, tiempoTareas
+    """
     for num in range(16):
         nombre=nombresTareas[num]
         duracion=duracionTareas[num]
@@ -189,5 +155,4 @@ def ingresarTareas(listaTareas,fechas,tiempoTareas):
         tarea = Tareas(nombre,duracion,dia,mes,año)
         listaTareas.append(tarea)
         #print(tarea)
-    return listaTareas, fechas, tiempoTareas
-
+    """
